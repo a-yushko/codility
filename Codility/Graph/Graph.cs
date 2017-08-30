8f6de8dd5b42ce3e56654ceca6d683eb468befbb
ayushko <aleksey.yushko@hotmail.com>
2017-08-30 23:38:10 +0300

    Implemented graph task.

---
+++ Codility/Graph/Graph.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment.Graph
+{
+    public class Graph
+    {
+        public List<Node> Nodes { get; }
+        public Graph()
+        {
+            Nodes = new List<Node>();
+        }
+
+        public void AddNode(Node node)
+        {
+            Nodes.Add(node);
+        }
+
+        /// <summary>
+        /// Given a directed graph, find out whether there is a route between two nodes
+        /// </summary>
+        /// <param name="n1">The first node</param>
+        /// <param name="n2">The second node</param>
+        /// <returns></returns>
+        public bool HasRoute(Node n1, Node n2)
+        {
+            bool hasRoute = false;
+            Queue<Node> q = new Queue<Node>();
+            n1.Visited = true;
+            q.Enqueue(n1);
+            while(q.Count > 0)
+            {
+                var node = q.Dequeue();
+                hasRoute = Visit(node, n2);
+                if (hasRoute)
+                    break;
+                foreach(var adjNode in node.Adjacent)
+                {
+                    if (!adjNode.Visited)
+                    {
+                        adjNode.Visited = true;
+                        q.Enqueue(adjNode);
+                    }
+                }
+            }
+            return hasRoute;
+        }
+
+        private bool Visit(Node n1, Node n2)
+        {
+            return n1.Id == n2.Id;
+        }
+    }
+
+    public class Node
+    {
+        public int Id { get; set; }
+        public List<Node> Adjacent { get; set; }
+        public bool Visited { get; set; }
+
+        public Node(int id)
+        {
+            Id = id;
+            Adjacent = new List<Node>();
+        }
+
+        public void AddAdjacent(Node node)
+        {
+            Adjacent.Add(node);
+        }
+    }
+}
---
+++ Tests/HasRouteTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Assesment.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class HasRouteTest
+    {
+        [TestMethod]
+        public void HasRouteTestCase1()
+        {
+            Graph graph = GetTestCase1();
+            Assert.IsTrue(graph.HasRoute(graph.Nodes[0], graph.Nodes[4]));
+            Assert.IsFalse(graph.HasRoute(graph.Nodes[3], graph.Nodes[1]));
+            Assert.IsFalse(graph.HasRoute(graph.Nodes[4], graph.Nodes[1]));
+        }
+
+        private Graph GetTestCase1()
+        {
+            Graph graph = new Graph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var n3 = new Node(3);
+            var n4 = new Node(4);
+            var n5 = new Node(5);
+
+            n1.AddAdjacent(n2);
+            n1.AddAdjacent(n3);
+            n2.AddAdjacent(n4);
+            n4.AddAdjacent(n5);
+            n5.AddAdjacent(n4);
+
+            graph.AddNode(n1);
+            graph.AddNode(n2);
+            graph.AddNode(n3);
+            graph.AddNode(n4);
+            graph.AddNode(n5);
+
+            return graph;
+        }
+    }
+}
