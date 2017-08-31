9dbc8dbeb8496fc8f351a6a94a0bb206e5657fa7
ayushko <aleksey.yushko@hotmail.com>
2017-08-31 12:00:18 +0300

    Added string test.

---
+++ Codility/Strings/StringTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Assesment.Strings
+{
+    public static class StringTask
+    {
+        /// <summary>
+        /// Determine if a string has all unique characters. What if you cannot use additional data structures?
+        /// </summary>
+        /// <param name="str">String to check</param>
+
+        public static bool AllUnique(string str)
+        {
+            Hashtable chars = new Hashtable();
+            foreach (char c in str)
+            {
+                var key = Char.ToUpper(c);
+                if (chars.ContainsKey(key))
+                    return false;
+                chars.Add(key, null);
+            }
+            return true;
+        }
+
+        public static bool AllUnique2(string str)
+        {
+            for (int i = 0; i < str.Length - 1; i++)
+                for (int j = i + 1; j < str.Length; j++)
+                {
+                    if (Char.ToUpper(str[i]) == Char.ToUpper(str[j]))
+                        return false;
+                }
+            return true;
+        }
+
+        public static bool AllUnique3(string str)
+        {
+            var charArray = str.ToUpper().ToArray();
+            Array.Sort(charArray);
+            for (int i = 0; i < charArray.Length - 1; i++)
+            {
+                if (Char.ToUpper(charArray[i]) == Char.ToUpper(charArray[i + 1]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Tests/StringTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assesment.Strings;
+
+namespace Tests
+{
+    /// <summary>
+    /// Summary description for StringTest
+    /// </summary>
+    [TestClass]
+    public class StringTest
+    {
+
+        [TestMethod]
+        public void TestAllUnique()
+        {
+            Assert.IsTrue(StringTask.AllUnique("throne"));
+            Assert.IsFalse(StringTask.AllUnique("character"));
+            Assert.IsTrue(StringTask.AllUnique(""));
+        }
+
+        [TestMethod]
+        public void TestAllUniqueCaseInsensitive()
+        {
+            Assert.IsFalse(StringTask.AllUnique("Stwopqadfs"));
+            Assert.IsFalse(StringTask.AllUnique("MOM"));
+        }
+
+        [TestMethod]
+        public void TestAllUnique2()
+        {
+            Assert.IsTrue(StringTask.AllUnique2("throne"));
+            Assert.IsFalse(StringTask.AllUnique2("character"));
+            Assert.IsTrue(StringTask.AllUnique2(""));
+        }
+
+        [TestMethod]
+        public void TestAllUnique2CaseInsensitive()
+        {
+            Assert.IsFalse(StringTask.AllUnique2("Stwopqadfs"));
+            Assert.IsFalse(StringTask.AllUnique2("ARR"));
+            Assert.IsFalse(StringTask.AllUnique2("aRtiFACT"));
+            Assert.IsTrue(StringTask.AllUnique2("aSTro"));
+        }
+
+        [TestMethod]
+        public void TestAllUnique3()
+        {
+            Assert.IsTrue(StringTask.AllUnique3("throne"));
+            Assert.IsFalse(StringTask.AllUnique3("character"));
+            Assert.IsTrue(StringTask.AllUnique3(""));
+        }
+
+        [TestMethod]
+        public void TestAllUnique3CaseInsensitive()
+        {
+            Assert.IsFalse(StringTask.AllUnique3("Stwopqadfs"));
+            Assert.IsFalse(StringTask.AllUnique3("ARR"));
+            Assert.IsFalse(StringTask.AllUnique3("aRtiFACT"));
+            Assert.IsTrue(StringTask.AllUnique3("aSTro"));
+        }
+    }
+}
