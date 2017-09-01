8ea819afb34301d0c00ed4d679aa92aac7d0f6de
ayushko <aleksey.yushko@hotmail.com>
2017-09-01 22:57:47 +0300

    Animal shelter implementation.

---
+++ Codility/LinkedList/AnimalQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Assesment.LinkedList
+{
+    /*An animal shelter, which holds only dogs and cats, operates on a strictly"first in, 
+    first out" basis. People must adopt either the "oldest" (based on arrival time) of 
+    all animals at the shelter, or they can select whether they would prefer a dog or a 
+    cat(and will receive the oldest animal of that type). They cannot select which 
+    specific animal they would like. Create the data structures to maintain this system 
+    and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat. */
+
+    [DebuggerDisplay("Count = {Count}")]
+    public class AnimalQueue
+    {
+        public void Enqueue(Animal animal)
+        {
+            _animals.AddLast(animal);
+        }
+
+        public Animal Dequeue()
+        {
+            if (_animals.First == null)
+                throw new QueueEmptyException();
+            var first = _animals.First.Value;
+            _animals.RemoveFirst();
+            return first;
+        }
+
+        public Dog DequeueDog()
+        {
+            return Dequeue<Dog>();
+        }
+
+        public Cat DequeueCat()
+        {
+            return Dequeue<Cat>();
+        }
+
+        public int Count => _animals.Count;
+
+        private T Dequeue<T>() where T : Animal
+        {
+            if (_animals.First == null)
+                throw new QueueEmptyException();
+            var animal = _animals.First;
+            while (animal != null)
+            {
+                if (animal.Value is T)
+                    break;
+                else
+                    animal = animal.Next;
+            }
+            if (animal == null)
+                throw new NotFoundException();
+            _animals.Remove(animal);
+            return animal.Value as T;
+        }
+
+        private LinkedList<Animal> _animals = new LinkedList<Animal>();
+    }
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+    }
+    public class Dog : Animal { }
+    public class Cat : Animal { }
+    public class QueueEmptyException : Exception { }
+    public class NotFoundException : Exception { }
+}
---
+++ Tests/AnimalShelterTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assesment.LinkedList;
+
+namespace Tests
+{
+    [TestClass]
+    public class AnimalShelterTest
+    {
+        [TestMethod]
+        public void TestDequeue()
+        {
+            var animals = GetTestCase1();
+            var oldest = animals.Dequeue();
+
+            Assert.IsInstanceOfType(oldest, typeof(Cat));
+            Assert.AreEqual(3, animals.Count);
+            Assert.AreEqual("Sam", oldest.Name);
+
+            oldest = animals.Dequeue();
+            Assert.IsInstanceOfType(oldest, typeof(Cat));
+            Assert.AreEqual(2, animals.Count);
+            Assert.AreEqual("Tom", oldest.Name);
+        }
+
+        [TestMethod]
+        public void TestDequeueCat()
+        {
+            var animals = GetTestCase1();
+            var oldest = animals.DequeueCat();
+
+            Assert.IsInstanceOfType(oldest, typeof(Cat));
+            Assert.AreEqual(3, animals.Count);
+        }
+
+        [TestMethod]
+        public void TestDequeueDog()
+        {
+            var animals = GetTestCase1();
+            var oldest = animals.DequeueDog();
+
+            Assert.IsInstanceOfType(oldest, typeof(Dog));
+            Assert.AreEqual(3, animals.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QueueEmptyException))]
+        public void TestDequeueEmpty()
+        {
+            var animals = new AnimalQueue();
+
+            Assert.AreEqual(0, animals.Count);
+
+            var oldest = animals.Dequeue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QueueEmptyException))]
+        public void TestDequeueCatEmpty()
+        {
+            var animals = new AnimalQueue();
+
+            Assert.AreEqual(0, animals.Count);
+
+            var oldest = animals.DequeueCat();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public void TestDequeueCatNotFound()
+        {
+            var animals = new AnimalQueue();
+            animals.Enqueue(new Dog() { Name = "Body" });
+
+            var oldest = animals.DequeueCat();
+        }
+
+        private AnimalQueue GetTestCase1()
+        {
+            AnimalQueue queue = new AnimalQueue();
+            queue.Enqueue(new Cat() { Name = "Sam" });
+            queue.Enqueue(new Cat() { Name = "Tom" });
+            queue.Enqueue(new Dog() { Name = "Tarly" });
+            queue.Enqueue(new Cat() { Name = "Messy" });
+            return queue;
+        }
+    }
+}
