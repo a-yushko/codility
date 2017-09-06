38adf7591bc91400c2c2db771e0790bc4da29bb3
ayushko <aleksey.yushko@hotmail.com>
2017-09-06 18:06:48 +0300

    Bit manipulation task

---
+++ Codility/BitManipulation/SequenceOfOnes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Assesment.BitManipulation
+{
+    public static class SequenceOfOnes
+    {
+        public static int CountSequence(int num)
+        {
+            if (num == 0)
+                return 1;
+            int start = 0;
+            int length = CountOnes(num, start);
+            int size = GetSize(num);
+            for (int i = 0; i < size; i++)
+            {
+                if (CheckBit(num, i))
+                    continue;
+                var count = CountOnes(FlipBit(num, i), start);
+                if (count > length)
+                    length = count;
+                start = i;
+            }
+            return length;
+        }
+        private static int GetSize(int num)
+        {
+            int size = Marshal.SizeOf(num) * 8;
+            for (int i = 0; i < size; i++)
+            {
+                if (CheckBit(num, size - 1 - i))
+                    return size - i;
+            }
+            return 1;
+        }
+        private static bool CheckBit(int num, int i)
+        {
+            return (num & (1 << i)) != 0;
+        }
+        private static int FlipBit(int num, int i)
+        {
+            return num ^ (1 << i);
+        }
+        private static int CountOnes(int num, int start)
+        {
+            int length = 0;
+            int count = 0;
+            for (int i = start; i < GetSize(num); i++)
+            {
+                if (!CheckBit(num, i))
+                {
+                    if (length < count)
+                        length = count;
+                    count = 0;
+                }
+                else
+                    count++;
+            }
+            if (length < count)
+                length = count;
+            return length;
+        }
+    }
+}
---
+++ Tests/SequenceOfOnesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assesment.BitManipulation;
+
+namespace Tests
+{
+    [TestClass]
+    public class SequenceOfOnesTest
+    {
+        [TestMethod]
+        public void TestSequenceTestCase1()
+        {
+            Assert.AreEqual(8, SequenceOfOnes.CountSequence(1775));
+            Assert.AreEqual(6, SequenceOfOnes.CountSequence(1773));
+            Assert.AreEqual(11, SequenceOfOnes.CountSequence(2047));
+            Assert.AreEqual(1, SequenceOfOnes.CountSequence(0));
+        }
+    }
+}
